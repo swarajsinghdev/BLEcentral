955f0751702e0bfb6828dc4717c43d5c2f349967
Swaraj <swarajmeet@crebos.online>
2025-12-15 16:11:58 +0530

    Initial Commit

---
+++ BLEcentral/BLEcentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEcentralApp.swift
+//  BLEcentral
+//
+//  Created by Swarajmeet Singh on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BLEcentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
